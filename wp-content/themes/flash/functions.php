a56130ea66f50544d4bbb16475ec7f5dc6f1b126
MartinH014 <designsbymartin014@gmail.com>
2018-04-17 23:51:41 -0500

    Removal of footer text

--- wp-content/themes/flash/functions.php
+++ wp-content/themes/flash/functions.php
@@ -313,3 +313,51 @@ require get_template_directory() . '/inc/class-flash-admin.php';
 if ( class_exists( 'TG_Demo_Importer' ) ) {
 	require get_template_directory() . '/inc/demo-config.php';
 }
+
+//copy below this line, you shouldn't include <?php
+//paste this code in your function.php
+//flash footer editor
+
+if (! function_exists('flash_custom_credits')) {
+	
+	add_action('init','remove_default_flash_credits');
+	function remove_default_flash_credits(){		
+		remove_action('flash_copyright_area','flash_footer_copyright');
+	}
+	
+	add_filter('flash_copyright_area', 'flash_custom_credits');
+	function flash_custom_credits(){
+		$credit_value ='<div class="copyright"><span class="copyright-text">Copyright &copy; ' . date('Y') . ' <a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" ><span>' . get_bloginfo( 'name', 'display' ) . '.</span></a> ' .get_option('flash_custom_credits'). '</span></div>';
+		echo $credit_value;
+	}
+	
+}
+
+
+if (! function_exists('flash_custom_options')) {
+	
+	add_action('customize_register', 'flash_custom_options', 100, 1);
+	function flash_custom_options( $wp_customize ) {
+		
+		$wp_customize->add_section('flash_footer_options', array(
+		'capabitity' => 'edit_theme_options',
+		//'priority' => 0,
+		'title' => __('Footer', 'flash')
+		));
+	  
+		$wp_customize->add_setting( 'flash_custom_credits' , array(
+		'default'     => '<br/>Configure in Appearance => Customize => Footer',
+		'transport'   => 'refresh',
+		'capability' => 'edit_theme_options',
+		'type'        => 'option',
+		) );
+
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'flash_custom_credits', array(
+		'label'        => __( 'Footer Text', 'flash' ),
+		'section' => 'flash_footer_options',
+		'settings'   => 'flash_custom_credits',
+		) ) );
+	}
+	
+}
+?>
